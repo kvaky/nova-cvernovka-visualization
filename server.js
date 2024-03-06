3d77230475d0509f96c41ac94b3a2a668d0a6800
kvaky <peter.kvackay@proton.me>
2024-03-06 06:29:57 +0100

    Error handling

--- server.js
+++ server.js
@@ -3,161 +3,109 @@ const passport = require('passport');
 const { google } = require('googleapis');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
-const fs = require('fs');
 require('dotenv').config();
 
-// Initialize Express
 const app = express();
 const port = process.env.PORT;
+const sheets = google.sheets({ version: 'v4', auth: process.env.GOOGLE_SHEETS_API_KEY });
 
-// Configure session
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true
-}));
-
-// Initialize Passport and restore authentication state from session
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Passport session setup
-passport.serializeUser((user, done) => {
-    done(null, user);
-});
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
 
-passport.deserializeUser((obj, done) => {
-    done(null, obj);
-});
+const fetchSheetData = async (spreadsheetId, sheetName) => {
+    try {
+        const response = await sheets.spreadsheets.values.get({
+            spreadsheetId,
+            range: `${sheetName}`,
+            valueRenderOption: 'UNFORMATTED_VALUE',
+        });
+        return response.data.values;
+    } catch (error) {
+        console.error('Error fetching sheet data:', error);
+        throw error;
+    }
+};
 
-const sheets = google.sheets({ version: 'v4', auth: process.env.GOOGLE_SHEETS_API_KEY });
-
-// Function to check if user is allowed
 const isUserAllowed = async (profile) => {
     try {
         const allowedUsersData = await fetchSheetData(process.env.SPREADSHEET_ID, "allowed_google_users");
         const allowedEmails = allowedUsersData.map(row => row[0]);
-
-        const userEmail = profile.emails[0].value;
-        return allowedEmails.includes(userEmail);
+        return allowedEmails.includes(profile.emails[0].value);
     } catch (error) {
-        console.error('Error checking if user is allowed:', error);
-        return false;
+        console.error('Error checking user permission:', error);
+        throw error;
     }
 };
 
-// Use Google OAuth 2.0 Strategy
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: '/auth/google/callback'
-},
-    async (accessToken, refreshToken, profile, done) => {
-        try {
-            if (await isUserAllowed(profile)) {
-                return done(null, profile);
-            } else {
-                return done(null, false, { message: 'Unauthorized user' });
-            }
-        } catch (error) {
-            console.error('Error checking if user is allowed:', error);
-            return done(error);
-        }
+}, async (accessToken, refreshToken, profile, done) => {
+    try {
+        if (await isUserAllowed(profile)) return done(null, profile);
+        return done(null, false, { message: 'Unauthorized user' });
+    } catch (error) {
+        return done(error);
     }
-));
+}));
 
-// Middleware to check authentication and allowed users
 const isAuthenticated = async (req, res, next) => {
-    if (req.isAuthenticated()) {
-        try {
-            if (await isUserAllowed(req.user)) {
-                return next();
-            } else {
-                req.logout(() => {
-                    res.redirect('/');
-                });
-            }
-        } catch (error) {
-            console.error('Error checking if user is allowed:', error);
-            return res.status(500).send('Error checking authentication');
-        }
-    } else {
-        res.redirect('/auth/google');
+    try {
+        if (req.isAuthenticated()) {
+            if (await isUserAllowed(req.user)) return next();
+            else req.logout(() => res.redirect('/login-error'));
+        } else res.redirect('/auth/google');
+    } catch (error) {
+        next(error);
     }
 };
 
-
-// Define routes
-
-// Google Authentication Routes
 app.get('/auth/google',
     passport.authenticate('google', { scope: ['email'] })
 );
 
 app.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/login-error' }),
-    (req, res) => {
-        // Successful authentication, redirect home.
-        res.redirect('/');
-    }
+    (req, res) => res.redirect('/')
 );
 
 app.get('/login-error', (req, res) => {
     res.send('Unauthorized access: You do not have permission to access this application.');
 });
 
-// Logout Route
 app.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
 });
 
-// Main Route - Requires Authentication
 app.get('/', isAuthenticated, (req, res) => {
     res.sendFile(__dirname + '/public/real_time_diagram.html');
     console.log(req.user);
 });
 
-const fetchSheetData = async (spreadsheetId, sheetName) => {
-    const response = await sheets.spreadsheets.values.get({
-        spreadsheetId,
-        range: `${sheetName}`,
-        valueRenderOption: 'UNFORMATTED_VALUE',
-    });
-    return response.data.values;
-};
-
-// Function to fetch data from Google Sheets
-async function fetchDataFromSpreadSheet(spreadsheetId, sheetsToGet = [], sheetsToExclude = []) {
-    let sheetNames;
-    if ((sheetsToGet.length !== 0) && (sheetsToExclude.length !== 0)){
-        throw new Error('Error: sheetsToGet and sheetsToExclude are both provided');
-    } else if (sheetsToExclude.length !== 0) {
-        const sheetMetadata = await sheets.spreadsheets.get({ spreadsheetId, fields: 'sheets.properties' });
-        sheetNames = sheetMetadata.data.sheets
-            .filter(sheet => !sheetsToExclude.includes(sheet.properties.title))
-            .map(sheet => sheet.properties.title);
-    } else {
-        sheetNames = sheetsToGet;
-    }
-
-    return await Promise.all(sheetNames.map(async (sheetName) => {
-        return { [sheetName]: await fetchSheetData(spreadsheetId, sheetName) };
-    })).then((data) => Object.assign({}, ...data));
-}
-
-// Data Fetching Route - Requires Authentication
-app.get('/fetch-data', isAuthenticated, async (req, res) => {
+app.get('/fetch-data', isAuthenticated, async (req, res, next) => {
     try {
-        const allSheetsData = await fetchDataFromSpreadSheet(process.env.SPREADSHEET_ID, undefined, ["allowed_google_users"]);
+        const sheetMetadata = await sheets.spreadsheets.get({ spreadsheetId: process.env.SPREADSHEET_ID, fields: 'sheets.properties' });
+        const sheetNames = sheetMetadata.data.sheets
+            .map(sheet => sheet.properties.title)
+            .filter(sheetName => sheetName !== "allowed_google_users");
+        const allSheetsData = await Promise.all(sheetNames.map(async (sheetName) => {
+            return { [sheetName]: await fetchSheetData(process.env.SPREADSHEET_ID, sheetName) };
+        })).then((data) => Object.assign({}, ...data));
         res.json(allSheetsData);
     } catch (error) {
-        console.error('The API returned an error: ' + error);
-        res.status(500).send('Error fetching data');
+        next(error);
     }
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
